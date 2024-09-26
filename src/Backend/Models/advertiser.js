d0231fb512c05c871a78b64481713b3c3b5642bd
Tonyy131 <tonyayman131@gmail.com>
2024-09-26 12:31:30 +0300

    some schemas

--- src/Backend/Models/advertiser.js
+++ src/Backend/Models/advertiser.js
@@ -1,2 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const advertiserSchema = new Schema(
+  {
+    UserName: {
+      type: String,
+      required: true,
+    },
+    Password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Advertiser = mongoose.model("Advertiser", advertiserSchema);
+module.exports = Advertiser;
--- src/Backend/Models/tourGuide.js
+++ src/Backend/Models/tourGuide.js
@@ -1,2 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const tourGuideSchema = new Schema(
+  {
+    UserName: {
+      type: String,
+      required: true,
+    },
+    Password: {
+      type: String,
+      required: true,
+    },
+    Email: {
+        type: String, //to check correct format in the front end
+        required: true,
+    }
+  },
+  { timestamps: true }
+);
+
+const TourGuide = mongoose.model("TourGuide", tourGuideSchema);
+module.exports = TourGuide;
