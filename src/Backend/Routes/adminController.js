d57f1aa38edd9d39708c93ad21b78617d012b9f0
AshlynDream <youssefsedkey522@gmail.com>
2024-09-28 19:30:13 +0300

    Added Hashing and input validation to create admin

--- src/Backend/Routes/adminController.js
+++ src/Backend/Routes/adminController.js
@@ -1,16 +1,40 @@
-const adminModel = require("../Models/admin.js"); 
+const adminModel = require("../Models/admin.js");
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
 
 // Creating an admin
 const createAdmin = async (req, res) => {
     try {
         const { Username, Password } = req.body; 
-        const admin = await adminModel.create({ UserName: Username, Password });
+
+        
+        if (!Username || !Password) {
+            return res.status(400).json({ message: "Username and Password are required" });
+        }
+
+       
+        if (/\s/.test(Username)) {
+            return res.status(400).json({ message: "Username should not contain spaces" });
+        }
+
+      
+        const existingAdmin = await adminModel.findOne({ UserName: Username });
+        if (existingAdmin) {
+            return res.status(400).json({ message: "Username already exists" });
+        }
+
+       
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(Password, saltRounds);
+
+       
+        const admin = await adminModel.create({ UserName: Username, Password: hashedPassword });
+
         res.status(200).json(admin);
     } catch (err) {
-        console.error(err); 
-        res.status(400).json({ message: "Can't create the seller" });
+        console.error(err);
+        res.status(400).json({ message: "Can't create the admin" });
     }
 };
 
-module.exports = { createAdmin};
+module.exports = { createAdmin };
