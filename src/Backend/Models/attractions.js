5a95f4e1d04431a25fc91ca2419d957e23f4f34f
Tonyy131 <tonyayman131@gmail.com>
2024-09-28 20:34:08 +0300

    ..

--- src/Backend/Models/attractions.js
+++ src/Backend/Models/attractions.js
@@ -2,21 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const { ObjectId } = require("mongodb");
 
-const attractionsSchema = new Schema(
+const attractionSchema = new Schema(
   {
-    Name: {
-      type: String,
-      required: true,
+    Location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    Category: {
-      type: ObjectId,
-      required: true,
-    },
-    tags: [String], // array of tags
-    description: String,
   },
   { timestamps: true }
 );
 
-const Attractions = mongoose.model("Attractions", attractionsSchema);
-module.exports = Attractions;
+const Attraction = mongoose.model("Attraction", attractionSchema);
+module.exports = Attraction;
---
+++ src/Backend/Models/itinerary.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { ObjectId } = require("mongodb");
+
+const itinerarySchema = new Schema(
+  {
+    Activities: {
+      type: [ObjectId],
+      required: true,
+    },
+    LocationsToVisit: {
+      type: [ObjectId],
+      required: true,
+    },
+    TimeLine: {
+      type: String,
+    },
+    Language: {
+      type: String,
+      required: true,
+    }, // array of tags
+    Price: { type: String, required: true },
+    AvailableDates: { type: [Date], required: true },
+    PickUpLocation: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+    DropOffLocation: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const Itinerary = mongoose.model("Itinerary", itinerarySchema);
+module.exports = Itinerary;
