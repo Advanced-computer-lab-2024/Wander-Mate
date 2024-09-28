1caa5f6c51291a48d6aa87d8dee8fc02d699aba2
Peter ragy <peterragy0902>
2024-09-28 19:49:02 +0300

    added CreateTourGuide

--- src/Backend/App.js
+++ src/Backend/App.js
@@ -13,6 +13,10 @@ const {
   updateSeller,
 } = require("./Routes/sellerController.js");
 
+
+const{
+  createTourGuide,
+}=require("./Routes/tourGuideController.js");
 const MongoURI = process.env.MONGO_URI;
 console.log(MongoURI);
 //App variables
@@ -39,3 +43,4 @@ app.post("/addSeller", createSeller);
 app.get("/readSeller/:id", readSeller);
 app.put("/updateSeller", updateSeller);
 app.post("/searchAttractions", searchAttractions);
+app.post("/createTourGuide", createTourGuide);
