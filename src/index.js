e2b58a3125acf3e3e857559fc5ff1cb3941b8879
Nadeem202020 <145388019+Nadeem202020@users.noreply.github.com>
2024-10-03 13:29:13 +0300

    created frontend files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+import Register from './Frontend/components/Register';
+
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+ReactDOM.render(<Register/>,document.querySelector("#root"));
