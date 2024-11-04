26ffbf04695a3ea9f5c094c5accafcf1b2cd0c4a
John Nagi <johnnalarja@gmail.com>
2024-11-04 21:10:39 +0200

    images in loginpage and registerPage

--- src/frontend_react/src/pages/loginPage.jsx
+++ src/frontend_react/src/pages/loginPage.jsx
@@ -1,6 +1,6 @@
-"use client";
+import React from "react";
 import bg from "../public/images/auth/bg-2.jpg";
-import slider1 from "../public/images/auth/slide-1.png";
+import slider1 from "../public/images/auth/apple.png";
 import slider2 from "../public/images/auth/slide-2.png";
 import slider3 from "../public/images/auth/slide-3.png";
 import LogInForm from "./login";
@@ -15,11 +15,11 @@ const LoginPage = () => {
   const { isRtl } = useThemeStore();
 
   return (
-    <div className="loginwrapper bg-background flex items-center min-h-screen overflow-hidden  w-full">
-      <div className="lg-inner-column  grid  lg:grid-cols-2 w-full flex-wrap justify-center  p-4 overflow-y-auto">
+    <div className="loginwrapper bg-background flex items-center min-h-screen overflow-hidden w-full">
+      <div className="lg-inner-column grid lg:grid-cols-2 w-full flex-wrap justify-center p-4 overflow-y-auto">
         <div
-          className="h-full w-full  bg-no-repeat bg-center bg-cover hidden lg:block rounded-xl"
-          style={{ backgroundImage: `url(${bg.src})` }}
+          className="h-full w-full bg-no-repeat bg-center bg-cover hidden lg:block rounded-xl"
+          style={{ backgroundImage: `url(${bg})` }}
         >
           <Swiper
             key={`${isRtl}-swiper`}
@@ -46,22 +46,22 @@ const LoginPage = () => {
           >
             <SwiperSlide>
               <div className="w-full h-full flex justify-center items-center">
-                <img src={slider1.src} alt="image" className="" />
+                <img src={slider1} alt="Slider Image 1" className="" />
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className="w-full h-full flex justify-center items-center">
-                <img src={slider2.src} alt="image" className="" />
+                <img src={slider2} alt="Slider Image 2" className="" />
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className="w-full h-full flex justify-center items-center">
-                <img src={slider3.src} alt="image" className="" />
+                <img src={slider3} alt="Slider Image 3" className="" />
               </div>
             </SwiperSlide>
           </Swiper>
         </div>
-        <div className=" px-4  py-5 flex justify-center items-center">
+        <div className="px-4 py-5 flex justify-center items-center">
           <div className="lg:w-[480px]">
             <LogInForm />
           </div>
--- src/frontend_react/src/pages/registerPage.jsx
+++ src/frontend_react/src/pages/registerPage.jsx
@@ -1,4 +1,4 @@
-"use client";
+import React from "react";
 import bg from "../public/images/auth/bg-2.jpg";
 import slider1 from "../public/images/auth/slide-1.png";
 import slider2 from "../public/images/auth/slide-2.png";
@@ -10,14 +10,16 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { useThemeStore } from "../store";
+
 const RegisterPage = () => {
   const { isRtl } = useThemeStore();
+
   return (
-    <div className="loginwrapper bg-background flex items-center min-h-screen  overflow-hidden w-full">
-      <div className="lg-inner-column grid  grid-cols-2 w-full  justify-center overflow-y-auto p-4">
+    <div className="loginwrapper bg-background flex items-center min-h-screen overflow-hidden w-full">
+      <div className="lg-inner-column grid grid-cols-2 w-full justify-center overflow-y-auto p-4">
         <div
-          className="col-span-1 h-full w-full  bg-no-repeat bg-center bg-cover hidden lg:block rounded-xl"
-          style={{ backgroundImage: `url(${bg.src})` }}
+          className="col-span-1 h-full w-full bg-no-repeat bg-center bg-cover hidden lg:block rounded-xl"
+          style={{ backgroundImage: `url(${bg})` }}
         >
           <Swiper
             key={`${isRtl}-swiper`}
@@ -44,22 +46,22 @@ const RegisterPage = () => {
           >
             <SwiperSlide>
               <div className="w-full h-full flex justify-center items-center">
-                <img src={slider1.src} alt="image" className="" />
+                <img src={slider1} alt="Slide 1" className="" />
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className="w-full h-full flex justify-center items-center">
-                <img src={slider2.src} alt="image" className="" />
+                <img src={slider2} alt="Slide 2" className="" />
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className="w-full h-full flex justify-center items-center">
-                <img src={slider3.src} alt="image" className="" />
+                <img src={slider3} alt="Slide 3" className="" />
               </div>
             </SwiperSlide>
           </Swiper>
         </div>
-        <div className="px-4 py-4 flex justify-center items-center  col-span-2 lg:col-span-1">
+        <div className="px-4 py-4 flex justify-center items-center col-span-2 lg:col-span-1">
           <div className="sm:w-[480px] w-full">
             <RegForm />
           </div>
