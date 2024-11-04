aea5b4c1bd135fe839941cbf29e3bb4cd82bab23
Tonyy131 <tonyayman131@gmail.com>
2024-11-04 17:16:29 +0200

    frontend_react

--- src/frontend_react/src/App.js
+++ src/frontend_react/src/App.js
@@ -1,36 +1,20 @@
-import "./App.css";
-import StartPage from "./components/screens/startPage";
-import Register1 from "./components/screens/register1";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import TouristRegister from "./components/screens/touristRegister";
-import SellerRegister from "./components/screens/sellerRegister";
-import TourGuideRegister from "./components/screens/tourGuideRegister";
-import AdvertiserRegister from "./components/screens/advertiserRegister";
-import SellerRegister2 from "./components/screens/sellerRegister2";
-import  TouristRegister2 from "./components/screens/touristRegister2";
-import  TourGuideRegister2 from "./components/screens/tourGuideRegister2";
-import   AdvertiserRegister2 from "./components/screens/advertiserRegister2";
-
+import logo from "./logo.svg";
+import "./assets/scss/globals.scss";
+import "./assets/scss/theme.scss";
+import { Button } from "./components/ui/button";
+import { themes } from "./config/thems";
 
 function App() {
   return (
-    <div className="container">
-      <Router>
-        <Routes>
-          <Route path="/" element={<StartPage />} />
-          <Route path="/registerStart" element={<Register1 />} />
-          <Route path="/touristRegister" element={<TouristRegister />} />
-          <Route path="/sellerRegister" element={<SellerRegister />} />
-          <Route path="/guideRegister" element={<TourGuideRegister />} />
-          <Route path="/advertiserRegister" element={<AdvertiserRegister />} />
-          <Route path="/sellerPassword" element={<SellerRegister2 />} />
-          <Route path="/touristPassword" element={<TouristRegister2 />} />
-          <Route path="/tourguidePassword" element={<TourGuideRegister2 />} />
-          <Route  path="/advertiserPassword" element={<AdvertiserRegister2 />} />
-
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <Button>Primary</Button>
+      <Button color="secondary">Secondary</Button>
+      <Button color="success">Success</Button>
+      <Button color="info">Info</Button>
+      <Button color="warning">Warning</Button>
+      <Button color="destructive">Danger</Button>
+      <Button color="dark">Dark</Button>
+    </>
   );
 }
 
