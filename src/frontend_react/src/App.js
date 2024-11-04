4401c4296ecd842a4a65e942c0addbf68b6dc24c
John Nagi <johnnalarja@gmail.com>
2024-11-04 22:13:57 +0200

    Tourist homePage semi done

--- src/frontend_react/src/App.js
+++ src/frontend_react/src/App.js
@@ -3,13 +3,14 @@ import "./assets/scss/theme.scss";
 import LoginPage from "./pages/loginPage";
 import RegisterPage from "./pages/registerPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import TouristHomePage from "./pages/TouristHomepage";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/loginPage" element={<LoginPage />} />
         <Route path="/registerPage" element={<RegisterPage />} />
+        <Route path="/TouristHomepage" element={<TouristHomePage/>}/>
       </Routes>
     </Router>
   );
