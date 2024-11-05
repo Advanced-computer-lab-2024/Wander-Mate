0d3829d953970c77baa5701c38ab5744cb9fb526
Tonyy131 <tonyayman131@gmail.com>
2024-11-05 20:00:05 +0200

    tweety table

--- src/frontend_react/src/App.js
+++ src/frontend_react/src/App.js
@@ -4,6 +4,7 @@ import LoginPage from "./pages/loginPage";
 import RegisterPage from "./pages/registerPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TouristHomePage from "./pages/TouristHomepage";
+import ShowWithDocs from "./components/showRegisteredWithDocs";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/loginPage" element={<LoginPage />} />
         <Route path="/registerPage" element={<RegisterPage />} />
         <Route path="/TouristHomepage" element={<TouristHomePage />} />
+        <Route path="/ShowWithDocs" element={<ShowWithDocs />} />
       </Routes>
     </Router>
   );
--- src/frontend_react/src/components/ui/table.jsx
+++ src/frontend_react/src/components/ui/table.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "../../lib/utils";
 
 const Table = React.forwardRef(({ className, wrapperClass, ...props }, ref) => (
   <div className={cn("overflow-x-auto", wrapperClass)}>
