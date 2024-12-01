45017a5721e5a9fd052607e508525611db5b5c83
Nadeem202020 <145388019+Nadeem202020@users.noreply.github.com>
2024-12-01 12:57:46 +0200

    Update payForFlight.jsx

--- src/frontend_react/src/components/payForFlight.jsx
+++ src/frontend_react/src/components/payForFlight.jsx
@@ -54,14 +54,25 @@ const PayNow = ({
   }, [paymentMethod]);
 
   const handleWallet = async () => {
-    if (userPoints < amount) {
-      setAlertMessage("Insufficient points to complete the transaction.");
-      return;
-    }
+    // if (userPoints < amount) {
+    //   setAlertMessage("Insufficient points to complete the transaction.");
+    //   return;
+    // }
+
+    // try {
+    //   const response = await axios.put("http://localhost:8000/payWithWallet", {
+    //     touristID,
+    //     amount,
+    //   });
+    const username = sessionStorage.getItem("username");
+    const reply = await fetch(`http://localhost:8000/getID/${username}`);
+    if (!reply.ok) throw new Error("Failed to get tourist ID");
+
+    const { userID } = await reply.json();
 
     try {
       const response = await axios.put("http://localhost:8000/payWithWallet", {
-        touristID,
+        touristID: userID,
         amount,
       });
       if (response.status === 200) {
