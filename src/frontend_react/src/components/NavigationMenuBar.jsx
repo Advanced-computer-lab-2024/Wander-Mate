3eae3961a2bd2a8f0c35139f90ba7a6c2f300556
Peter ragy <peterragy0902@gmail.com>
2024-12-03 20:33:52 +0200

    many lots too much gamadan from bedo

--- src/frontend_react/src/components/NavigationMenuBar.jsx
+++ src/frontend_react/src/components/NavigationMenuBar.jsx
@@ -32,6 +32,7 @@ import {
   Trash2,
   Bell,
   FileText,
+  ListOrdered
 } from "lucide-react";
 import { ScrollArea } from "./ui/scroll-area";
 import { toast } from "./ui/use-toast";
@@ -384,6 +385,9 @@ const NavigationMenuBar = ({ likedItemsCount = 0 }) => {
   const goToHistory = async () => {
     navigate("/touristHistory");
   };
+  const goToOrders = async () => {
+    navigate("/touristOrders");
+  };
   const deleteNotification = useCallback(
     async (notificationId) => {
       try {
@@ -732,6 +736,11 @@ const NavigationMenuBar = ({ likedItemsCount = 0 }) => {
                   <DropdownMenuItem>
                     <History className="mr-2 h-4 w-4" />
                     <span onClick={goToHistory}>History</span>
+                    <DropdownMenuShortcut>⌘H</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <ListOrdered className="mr-2 h-4 w-4" />
+                    <span onClick={goToOrders}>Orders</span>
                     <DropdownMenuShortcut>⌘H</DropdownMenuShortcut>
                   </DropdownMenuItem>
                 </DropdownMenuGroup>
