73c484a8899c80f67fbc21283accb681b949e13c
Nadeem202020 <145388019+Nadeem202020@users.noreply.github.com>
2024-12-08 22:15:23 +0200

    new updates

--- src/frontend_react/src/components/tourismGovernerFooter.jsx
+++ src/frontend_react/src/components/tourismGovernerFooter.jsx
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom";
 import { Icon } from "@iconify/react";
 const TourismGovernerFooter = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8 mt-10">
+    <footer
+      style={{ backgroundColor: "#826AF9" }}
+      className="text-white py-8 mt-10"
+    >
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap justify-between">
           <div className="w-full md:w-1/3 mb-6 md:mb-0">
             <h3 className="text-lg font-semibold mb-2">About WanderMate</h3>
-            <p className="text-gray-400">Your Traveling Best Mate</p>
+            <p className="text-white-400">Your Traveling Best Mate</p>
           </div>
           <div className="w-full md:w-1/3 mb-6 md:mb-0">
             <h3 className="text-lg font-semibold mb-2">Quick Links</h3>
@@ -33,20 +36,29 @@ const TourismGovernerFooter = () => {
           <div className="w-full md:w-1/3">
             <h3 className="text-lg font-semibold mb-2">Connect With Us</h3>
             <div className="flex space-x-4">
-              <a href="#" className="text-gray-400 hover:text-white">
+              <a
+                href="https://www.facebook.com/profile.php?id=61570101207724&mibextid=LQQJ4d&mibextid=LQQJ4d"
+                className="text-gray-400 hover:text-white"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <Icon icon="mdi:facebook" className="text-2xl" />
               </a>
-              <a href="#" className="text-gray-400 hover:text-white">
-                <Icon icon="mdi:twitter" className="text-2xl" />
-              </a>
-              <a href="#" className="text-gray-400 hover:text-white">
+              <a
+                href="https://www.instagram.com/wander__mate?igsh=a2UxNWFiYXcxZWw2"
+                className="text-gray-400 hover:text-white"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <Icon icon="mdi:instagram" className="text-2xl" />
               </a>
             </div>
           </div>
         </div>
         <div className="mt-8 text-center text-gray-400">
-          <p>&copy; 2024 WanderMate. All rights reserved.</p>
+          <p className="text-white">
+            &copy; 2024 WanderMate. All rights reserved.
+          </p>
         </div>
       </div>
     </footer>
